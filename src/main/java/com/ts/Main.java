8a7b5c19c1f2abdb609944ef1816a0b5bfaecb0c
Tushar Shete <tusharshetek@gmail.com>
2024-02-13 00:59:39 +0530

    PROB-1 | Update: Array index problem.

--- src/main/java/com/ts/Main.java
+++ src/main/java/com/ts/Main.java
@@ -1,8 +1,23 @@
 package com.ts;
 
+/**
+ * <a href="https://leetcode.com/problems/two-sum">Two sum</a>
+ */
 public class Main {
 
   public static void main(String[] args) {
-    System.out.print("Hello and welcome!");
+    twoSum(new int[]{2, 7, 11, 15}, 9);
+  }
+
+  public static int[] twoSum(int[] nums, int target) {
+    int length = nums.length;
+    for (int j = 0; j < length; j++) {
+      for (int i = 1; i < length; i++) {
+        if ((i != j) && target == (nums[j] + nums[i])) {
+          return new int[]{j, i};
+        }
+      }
+    }
+    return new int[]{};
   }
 }
